afa9a2c178f574fa421071818ddeb65ca66f8792
Mike Coletta <coletta.mike@gmail.com>
2021-05-12 17:06:35 -0700

    Added rel=noreferrer

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -9,19 +9,19 @@ function Contact() {
             </div>
             <div className="row iconRow">
                 <div className="card col-6 iconCard">
-                    <a href="https://www.linkedin.com/in/mike-coletta/" target="_blank"><span
+                    <a href="https://www.linkedin.com/in/mike-coletta/" target="_blank" rel="noreferrer"><span
                             className="fa fa-linkedin fa-5x"></span></a>
                 </div>
                 <div className="card col-6 iconCard">
-                    <a href="https://github.com/MikeColetta" target="_blank"><span
+                    <a href="https://github.com/MikeColetta" target="_blank" rel="noreferrer"><span
                             className="fa fa-github githubContactLink fa-5x"></span></a>
                 </div>
                 <div className="card col-6 iconCard">
-                    <a href="mailto:coletta.mike@gmail.com" target="_blank"><span
+                    <a href="mailto:coletta.mike@gmail.com" target="_blank" rel="noreferrer"><span
                             className="fa fa-envelope-o fa-5x"></span></a>
                 </div>
                 <div className="card col-6 iconCard">
-                    <a href="tel:+1-253-315-0112" target="_blank"><span className="fa fa-phone fa-5x"></span></a>
+                    <a href="tel:+1-253-315-0112" target="_blank" rel="noreferrer"><span className="fa fa-phone fa-5x"></span></a>
                 </div>
             </div>
             
