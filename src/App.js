4a32287b89cf4d4ee4a6a0bc0a3608ed6047afe1
Mike Coletta <coletta.mike@gmail.com>
2021-05-12 18:55:39 -0700

    Added switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import About from "./pages/About/About"
@@ -13,11 +13,13 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
+      <Switch>
         <Route exact path="/" component={About} />
         <Route exact path="/about" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/experience" component={Experience} />
         <Route exact path="/contact" component={Contact} />
+      </Switch>
       <Footer />
     </Router>
   );
