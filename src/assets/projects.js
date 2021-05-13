47bd00f44641d9d8b510cbf924bf46597ccd6d56
Mike Coletta <coletta.mike@gmail.com>
2021-05-12 17:43:28 -0700

    Added technologies list

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -14,6 +14,7 @@ const portfolioProjects = [
         description: `An employee directory application built in React. 
         The application pulls between 10-20 users using the Random User Generator API. 
         You can then filter and sort users by hire date, first name, last name, email and phone number.`,
+        tech: "React.js, Random User Generator API, Javascript",
         link: "https://mikecoletta.github.io/Employee-Directory/",
         github: "https://github.com/MikeColetta/Employee-Directory"
     },
@@ -26,6 +27,7 @@ const portfolioProjects = [
         It uses the reddit.images NPM package to pull a random meme. Users can see other users
         reviews and all of the reviews
         are stored in a MySQL database.`,
+        tech: "Reddit.images API, Sequelize, Bcrypt, DOTENV, MySQL2, Express, Handlebars, Session, MySQL",
         link: "https://bootcamp-project2-uw.herokuapp.com/",
         github: "https://github.com/kharam/Dank-Reviews"
     },
@@ -35,6 +37,7 @@ const portfolioProjects = [
         heading: "Track your expenses online and offline!",
         description: `This online/offline budget tracker uses IndexedDB to store user
          input data offline, so when the user is back online, the IndexedDB data is added to the MongoDB.`,
+        tech: "IndexedDB, MongoDB, Javascript, Express",
         link: "https://thawing-mesa-54023.herokuapp.com/",
         github: "https://github.com/MikeColetta/Online-Offline-Budget-Trackers"
     },
@@ -44,18 +47,9 @@ const portfolioProjects = [
         heading: "Find your forever home!",
         description: `The Relocator is an app for users that want to compare two cities. The app compares the location, cost of living, and weather data of two cities. The
         Relocator also stores previous user searches for later use.`,
+        tech: "W3 CSS Framework, JQuery, Open Weather API, Numbeo API, OpenStreetMap API, MapBox, Day.js",
         link: "https://mikecoletta.github.io/Group-Project-One/",
         github: "https://github.com/MikeColetta/Group-Project-One"
-    },
-    {
-        title: "Coding Quiz",
-        img: codingQuiz,
-        heading: "Test your coding knowledge!",
-        description: `When the player starts the game they have 75 seconds to
-        complete the quiz. Every wrong answer takes time off the clock. The final score is the time
-        left on the clock. High scores are saved to local storage.`,
-        link: "https://mikecoletta.github.io/Code-Quiz/",
-        github: "https://github.com/MikeColetta/Code-Quiz"
     },
     {
         title: "Weather Dashboard",
@@ -65,8 +59,20 @@ const portfolioProjects = [
         weather and the five day forecast for a searched city. The The UV index is
         color coded based on the severity level. The user's search history is
         saved in local storage.`,
+        tech: "HTML5, Bootstrap, Javascript, Open Weather API",
         link: "https://mikecoletta.github.io/Weather-Dashboard/",
         github: "https://github.com/MikeColetta/Weather-Dashboard"
+    },
+    {
+        title: "Coding Quiz",
+        img: codingQuiz,
+        heading: "Test your coding knowledge!",
+        description: `When the player starts the game they have 75 seconds to
+        complete the quiz. Every wrong answer takes time off the clock. The final score is the time
+        left on the clock. High scores are saved to local storage.`,
+        tech: "HTML5, Bootstrap, Javascript",
+        link: "https://mikecoletta.github.io/Code-Quiz/",
+        github: "https://github.com/MikeColetta/Code-Quiz"
     }
 ]
 
